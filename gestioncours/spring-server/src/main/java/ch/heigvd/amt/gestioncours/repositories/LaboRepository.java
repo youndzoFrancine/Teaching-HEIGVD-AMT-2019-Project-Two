190938e44d39cb564c8960f52bd4495cfa82b017
crescy <crescy@yahoo.com>
2020-01-09 12:29:02 +0100

    uls

--- gestioncours/spring-server/src/main/java/ch/heigvd/amt/gestioncours/repositories/LaboRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.heigvd.amt.gestioncours.repositories;
-
-import ch.heigvd.amt.gestioncours.api.model.Labo;
-import ch.heigvd.amt.gestioncours.entities.LaboEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LaboRepository extends CrudRepository<LaboEntity, String> {
-}
