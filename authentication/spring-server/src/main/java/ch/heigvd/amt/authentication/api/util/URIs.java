41950aedcbdc74de436261093553dcfb4892f21e
Crescence Yimnaing <37827277+CrescenceK@users.noreply.github.com>
2020-01-19 22:00:28 +0100

    resolve pb getting users without JWT

---
+++ authentication/spring-server/src/main/java/ch/heigvd/amt/authentication/api/util/URIs.java
@@ -0,0 +1,17 @@
+package ch.heigvd.amt.authentication.api.util;
+
+public interface URIs {
+
+    String AUTH = "/login";
+    String SWAGGER_HTML = "/swagger-ui.html";
+    String SWAGGER_UI_RESOURCES = "/webjars/springfox-swagger-ui/";
+    String SWAGGER_RESOURCES = "/swagger-resources";
+    String V2_API_DOCS = "/api-docs";
+    String DOCUMENTATION = "/";
+    String CREATE_USER = "/register";
+    String BLOCK = "/users/block/";
+    String UNBLOCK = "/users/unblock/";
+    String FORGOT_PASSWORD = "/reset_password";
+
+
+}
